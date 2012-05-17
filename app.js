50d3ec6d127ef07c8386507713f5da16663547ca
paulgreg <paulgreg@gmail.com>
2012-05-17 22:14:43 +0200

    After some refactoring

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ function is_int(value){
     return ((parseFloat(value) == parseInt(value)) && !isNaN(value));
 }
 
+var getNumbers = function(question) {
+    var parts = /((, )?(\d+))*$/.exec(question);
+    return parts[0].split(',');
+}
+
 /* Reimplement this function to answer questions. */
 var answer = function(question, req, res) {
 
@@ -12,25 +17,22 @@ var answer = function(question, req, res) {
     }
 
     if(question.indexOf('plus') != -1) {
-        var re = /([0-9]+) plus ([0-9]+)/ 
-        var nb = re.exec(question);
+        var nb= /(\d+)\D+(\d+)$/.exec(question);
         return ''+(parseInt(nb[1], 10) + parseInt(nb[2], 10));
     }
 
     if(question.indexOf('minus') != -1) {
-        var re = /([0-9]+) minus ([0-9]+)/ 
-        var nb = re.exec(question);
+        var nb = /(\d+)\D+(\d+)$/.exec(question);
         return ''+(parseInt(nb[1], 10) - parseInt(nb[2], 10));
     }
 
     if(question.indexOf('multiplied') != -1) {
-        var re = /([0-9]+)\D*([0-9]+)$/ 
-        var nb = re.exec(question);
+        var nb = /(\d+)\D*(\d+)$/.exec(question);
         return ''+ (parseInt(nb[1], 10) * parseInt(nb[2], 10));
     }
 
     if(question.indexOf('largest') != -1) {
-        var nb = question.split(' ');
+        var nb = getNumbers(question);
         var largest = 0;
         for(var i = 0; i < nb.length; i++) {
             var n = parseInt(nb[i], 10);
@@ -40,46 +42,44 @@ var answer = function(question, req, res) {
     }
 
     if(question.indexOf('square and a cube') != -1) {
-        var q = question.split(':')[1];
-        var nb = q.split(' ');
-        console.log('length', nb.length, 'nb', nb);
+        var nb = getNumbers(question);
         var largest = 0;
         for(var i = 0; i < nb.length; i++) {
             var n = parseInt(nb[i], 10);
-            if (!isNaN(n)) {
-                var sqr = Math.sqrt(n);
-                var isSqr = is_int(sqr);
-                var cube = Math.pow(n, 1/3);
-                var isCube = is_int(cube);
-
-                console.log('n', n, sqr, cube, isSqr, isCube);
-
-                if (isSqr && isCube) return ''+n;
-            }
+            var sqr = Math.sqrt(n);
+            var isSqr = is_int(sqr);
+            var cube = Math.pow(n, 1/3);
+            var isCube = is_int(cube);
+            if (isSqr && isCube) return ''+n;
         }
         return '';
     }
 
     if(question.indexOf('primes') != -1) {
-        var q = question.split('primes:')[1];
-        var nb = q.split(',');
+        var nb = getNumbers(question);
         for(var i = 0; i < nb.length; i++) {
             var n = parseInt(nb[i], 10);
-            if (!isNaN(n)) {
-                //console.log(question, 'prime', n);
-                var isPrime = true;
-                if (n > 1) {
-                    for(var i = 2; i <= n/2; i++) {
-                        if (n != i && n % i == 0)
-                            isPrime = false;
-                            break;
-                    }
+            var isPrime = true;
+            if (n > 1) {
+                for(var i = 2; i <= n/2; i++) {
+                    if (n != i && n % i == 0)
+                        isPrime = false;
+                        break;
                 }
-                if (isPrime)
-                    return n;
+            }
+            if (isPrime) {
+                return n;
             }
         }
         return '';
+    }
+
+    if(question.indexOf('Fibonacci') != -1) {
+        var nb = /(\d+)th/g.exec(question);
+        var fibo = function(i) {
+            return (i < 2) ?  i : fibo(i-2) + fibo(i-1);
+        }
+        return ''+fibo(nb[1]);
     }
 
     if(question.indexOf('Spain') != -1) {
@@ -90,32 +90,6 @@ var answer = function(question, req, res) {
     }
     if(question.indexOf('Eiffel') != -1) {
         return "Paris";
-    }
-    if(question.indexOf('Fibonacci') != -1) {
-        var q = question.split(':')[1];
-        var nb = q.split(' ');
-        console.log('length', nb.length, 'nb', nb);
-        var largest = 0;
-        for(var i = 0; i < nb.length; i++) {
-            var n = parseInt(nb[i], 10);
-            if (!isNaN(n)) {
-                var fibo = function(i){
-                    switch(i){
-                        case 0: return '0';
-                        case 1: return '1';
-                        case 2: return '1';
-                        case 3: return '2';
-                        case 4: return '3';
-                        case 5: return '5';
-                        case 6: return '8';
-                        case 7: return '13';
-                        case 6: return '8';
-                        case 6: return '8';
-                    }
-                }
-            }
-        }
-        return '';
     }
 
     return 'I dont know';
--- test/test.js
+++ test/test.js
@@ -2,45 +2,48 @@ var assert = require('assert');
 
 var app = require('../app.js');
 
-describe('App', function() {
+describe('Extreme Startup App', function() {
     it('should return our name', function() {
-        assert.equal(app('What is your name?'), 'MiraGreg');
+        assert.equal(app('308f59b0: What is your name?'), 'MiraGreg');
     });
     it('should return our name for another question', function() {
-        assert.equal(app('my name is hob. what is my name'), 'MiraGreg');
+        assert.equal(app('308f59b0: my name is hob. what is my name'), 'MiraGreg');
     });
     it('should return adition result', function() {
-        assert.equal(app('what is 9 plus 0'), '9');
+        assert.equal(app('308f59b0: what is 9 plus 0'), '9');
     });
     it('should return another adition result', function() {
-        assert.equal(app('babfbd50 what is 9 plus 10'), '19');
+        assert.equal(app('babfbd50: what is 9 plus 10'), '19');
     });
     it('should return minus result', function() {
-        assert.equal(app('what is 9 minus 2'), '7');
+        assert.equal(app('308f59b0: what is 9 minus 2'), '7');
     });
     it('should return multiply result', function() {
-        assert.equal(app('what is 9 multiplied by 2'), '18');
+        assert.equal(app('308f59b0: what is 9 multiplied by 2'), '18');
     });
-    it('should return multiply result', function() {
+    it('should return another multiply result', function() {
         assert.equal(app('babfbd50: what is 11 multiplied by 33'), '363');
     });
     it('should return largest number', function() {
-        assert.equal(app('blablabla largest: 88, 879, 866, 44'), '879');
+        assert.equal(app('32413g4e: which of theses is the largest: 88, 879, 866, 44'), '879');
     });
     it('should return nb both square and a cube', function() {
         assert.equal(app('1d7b16f0: which of the following numbers is both a square and a cube: 666, 25'), '');
     });
-    it('should return peseta', function() {
+    it('should return fibonacci result for 4', function() {
+        assert.equal(app("2c1e1c10: what is the 4th number in the Fibonacci sequence"), "3");
+    });
+    it('should return fibonacci result for 6', function() {
+        assert.equal(app("2c1e1c10: what is the 6th number in the Fibonacci sequence"), "8");
+    });
+    it('should return Spain currency', function() {
         assert.equal(app('593ef490: what currency did Spain use before the Euro'), "peseta");
     });
-    it('should return prime number', function() {
+    it('should return answer to prime number', function() {
         assert.equal(app("308f59b0: which of the following numbers are primes: 151, 542"), '151');
     });
-    it('should return banana', function() {
-        assert.equal(app('banana'), "yellow");
-    });
-    it('should return fibonacci', function() {
-        assert.equal(app("2c1e1c10: what is the 6th number in the Fibonacci sequence"), "");
+    it('should return color of a banana', function() {
+        assert.equal(app('308f59b0: What is the color of a banana ?'), "yellow");
     });
 });
 
